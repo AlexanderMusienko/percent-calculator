3b080b0cc8c916bfe42d297d0b5f4154dbea126e
Alexander Musienko <alexmvid0@gmail.com>
2024-06-15 01:57:35 +0300

    fixed theme problems

--- src/common/contexts/theme.context.tsx
+++ src/common/contexts/theme.context.tsx
@@ -1,25 +1,35 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { EThemeMode } from "../types/theme.type";
 
 type ThemeContextType = {
   themeMode: EThemeMode;
-  setThemeMode: (themeMode: EThemeMode) => void;
+  changeThemeMode: (themeMode: EThemeMode) => void;
 };
 
 export const ThemeContext = createContext<ThemeContextType>({
   themeMode: EThemeMode.DARK,
-  setThemeMode: () => {},
+  changeThemeMode: () => {},
 });
+
+const changeHtmlRootThemeMode = (newThemeMode: EThemeMode) => {
+  document.documentElement.classList.remove(EThemeMode.DARK, EThemeMode.LIGHT);
+  document.documentElement.classList.add(newThemeMode);
+};
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const [themeMode, setThemeMode] = useState<EThemeMode>(EThemeMode.DARK);
 
   const changeThemeMode = (newThemeMode: EThemeMode) => {
     setThemeMode(newThemeMode);
+    changeHtmlRootThemeMode(newThemeMode);
   };
 
+  useEffect(() => {
+    changeHtmlRootThemeMode(themeMode);
+  }, [themeMode]);
+
   return (
-    <ThemeContext.Provider value={{ themeMode, setThemeMode: changeThemeMode }}>
+    <ThemeContext.Provider value={{ themeMode, changeThemeMode }}>
       {children}
     </ThemeContext.Provider>
   );
