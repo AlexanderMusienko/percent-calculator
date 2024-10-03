61fbc705c5d85c65d36eb373ed0de810cbd8f350
Alexander Musienko <alexmvid0@gmail.com>
2024-10-04 00:00:26 +0300

    added cheat mechanic for easter egg

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,39 @@ import { PercentCalc } from "./domains/calculator/pages/percent-calc";
 import { PageLayout } from "./shared/layout/PageLayout";
 import { useContext, useEffect } from "react";
 import { NUIThemeWrapper } from "./shared/components/NextUIThemeWrapper";
-import { Flip, ToastContainer } from "react-toastify";
+import { Flip, toast, ToastContainer } from "react-toastify";
 import { ThemeContext } from "./common/contexts/theme.context";
 import "./styles/index.css";
 import "react-toastify/dist/ReactToastify.css";
+
+const CHEAT_CODE = "DUMBMODE";
+let currentInput = "";
+
+const onKeypress = (e: KeyboardEvent) => {
+  currentInput += e.key;
+
+  // Check if the current input ends with the cheat code
+  if (currentInput.toUpperCase().endsWith(CHEAT_CODE)) {
+    toast(window.dumbMode ? "Dumb mode off" : "Dumb mode on");
+    // Add your cheat code logic here
+    window.dumbMode = !window.dumbMode; // Example: Activate dumb mode
+    currentInput = ""; // Reset the input
+  }
+
+  // Keep only the last 5 characters (length of CHEAT)
+  if (currentInput.length > CHEAT_CODE.length) {
+    currentInput = currentInput.slice(-CHEAT_CODE.length);
+  }
+};
 
 function App() {
   const { themeMode } = useContext(ThemeContext);
 
   useEffect(() => {
     window.dumbMode = false;
+    document.addEventListener("keypress", onKeypress);
+
+    return () => document.removeEventListener("keypress", onKeypress);
   }, []);
 
   return (
